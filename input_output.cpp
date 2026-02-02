2a3f0aa1fd4624f5f0db0297855a58aacd37f124
Rajnish Kumar Singh <rajnishkrsingh801@gmail.com>
2026-02-02 05:39:14 -0500

    Add files via upload

---
+++ input_output.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a ;
+    cout<<"Enter the value\n";
+    cin>>a;
+    if(a%2==0){
+        cout<<"Even\n";
+    }else{
+        cout<<"odd\n";
+    }
+    return 0;
+
+}
---
+++ loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i ;
+    for (int i = 5; i < 50; i = i +1,"\n")
+    {
+        cout<<i<<"\n";
+
+    }
+    
+}
